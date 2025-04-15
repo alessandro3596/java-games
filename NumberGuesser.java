7bd47a0b1ec2c9e16b3a1dd5a98e218c096ee45c
Alessandro <blendergraphics133@gmail.com>
2025-04-15 12:41:25 +0530

    The class name has to be the same as the file name lol

--- NumberGuesser.java
+++ NumberGuesser.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class NumberGuessingGame {
+public class NumberGuesser {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
